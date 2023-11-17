1d07555195539b5c0434a60fb651afff5b5926e9
Brandon Barnett <bbarnett12h@gmail.com>
2023-11-17 16:21:54 -0500

    changes

--- JSONactivity.js
+++ JSONactivity.js
@@ -1,4 +1,4 @@
-fetch("https://api.punkapi.com/v2/beers/random?authuser=0&hl=en")
+fetch("https://api.punkapi.com/v2/beers/random")
 .then((response) => {
     // const jsonResponse = response.json()
     return response.json()
@@ -8,8 +8,16 @@ const  drinkContainer = document.getElementById("drink-container")
 
 json.forEach((drink) => {
     const headerTag = document.createElement ("h1")
-    headerTag.innerText = `${drink.name} ${drink.description} ${drink.img}`
+    const textTag = document.createElement ("p")
+    const imageTag = document.createElement ("img")
+    headerTag.innerText = `${drink.name}`
+    textTag.innerText = `${drink.description}`
+    imageTag.src=drink.image_url
     drinkContainer.appendChild(headerTag)
+    drinkContainer.appendChild(textTag)
+    drinkContainer.appendChild(imageTag)
+    headerTag.classList.add ("h1")
+    textTag.classList.add ("img")
 })
 })
 .catch((error) => {
